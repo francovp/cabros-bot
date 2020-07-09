faf5d4347f282581ad2f99055c88b1eee0af2b93
Franco Valerio <franco.gvp@hotmail.com>
2020-07-09 19:23:56 -0400

    feat(index): añadir test hello word

---
+++ index.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'test') {
+    msg.reply('Hello world!');
+  }
+});
+
+client.login(process.env.TOKEN);
